b38dceb26cd2a0f6cafdb4c9cf6b40f4fca5f322
Maheer Akbar <maheerakbar@gmail.com>
2024-08-05 03:33:33 +0500

    changes

--- app/Http/Controllers/ManhwaController.php
+++ app/Http/Controllers/ManhwaController.php
@@ -217,6 +217,13 @@ class ManhwaController extends Controller
         "post_id" => $post->id,
         "meta_key" => "_manga_total_votes",
         "meta_value" => 1,
+      ],
+      [
+        "post_id" => $post->id,
+        "meta_key" => "_wp_manga_chapters_warning",
+        "meta_value" => "<strong>Get daily updates and  Fasted uploads for  Manhwa and Manhua  with the best quality on Manhwa Collection (manhwacollection.com).</strong>
+
+&nbsp;",
       ]
     ];
 
