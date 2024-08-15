9a4aa375640327dd097887094c5110bfb8eeb045
Maheer Akbar <maheerakbar@gmail.com>
2024-08-15 12:58:30 +0500

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('chapter:check-delete')->dailyAt('00:00')->withoutOverlapping();
+        // $schedule->command('chapter:check-delete')->dailyAt('00:00')->withoutOverlapping();
         $schedule->command('crawl:manhwa-chapters')->withoutOverlapping()->everyTenMinutes();
         $schedule->command('fetch:chapter-images')->withoutOverlapping()->everyTenMinutes();
 
