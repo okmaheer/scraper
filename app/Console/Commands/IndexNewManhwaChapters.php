8fe2931ccac85e73662e1fdab6d5233fd7bfa676
Maheer Akbar <maheerakbar@gmail.com>
2024-08-21 14:12:11 +0500

    changes

--- app/Console/Commands/IndexNewManhwaChapters.php
+++ app/Console/Commands/IndexNewManhwaChapters.php
@@ -7,6 +7,7 @@ use App\Models\Manhwa;
 use Illuminate\Console\Command;
 use App\Models\Manwha;
 use App\Services\GoogleIndexingService;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Log;
 
@@ -28,47 +29,49 @@ class IndexNewManhwaChapters extends Command
     {
         // Fetch all manhwa
         $manhwaList = Manhwa::all();
-
+    
         if ($manhwaList->isEmpty()) {
             Log::info('No manhwa found.');
-
             $this->info('No manhwa found.');
             return;
         }
-
+    
         foreach ($manhwaList as $manhwa) {
-            // Fetch the latest chapter for each manhwa that hasn't been indexed yet
-            $chapter = $manhwa->chapters()
-                ->orderByRaw('CAST(chapter_number AS UNSIGNED) DESC')
-                ->first();
-
-            if (!$chapter->is_indexed) {
-                $chapterNumberFormatted = str_replace('.', '-', $chapter->chapter_number);
-                $slug = Str::slug("Chapter " . $chapterNumberFormatted);
-
-                $url = 'https://manhwacollection.com/manga/'.Str::slug($manhwa->name).'/'.$slug;
-                $response = $this->googleIndexingService->submitUrl($url);
-                if (isset($response['urlNotificationMetadata'])) {
-                    // Mark the chapter as indexed
-                    IndexChapters::create([
-                        'chapter_id'=>$chapter->id
-                    ]);
-                    $chapter->is_indexed = true;
-                    $chapter->save();
-
-                    Log::info($manhwa->id." Indexed: " . $url);
-
-                    $this->info($manhwa->id." Indexed: " . $url);
+            DB::transaction(function () use ($manhwa) {
+                // Fetch the latest chapter for each manhwa that hasn't been indexed yet
+                $chapter = $manhwa->chapters()
+                    ->orderByRaw('CAST(chapter_number AS UNSIGNED) DESC')
+                    ->first();
+    
+                if (!$chapter->is_indexed) {
+                    $chapterNumberFormatted = str_replace('.', '-', $chapter->chapter_number);
+                    $slug = Str::slug("Chapter " . $chapterNumberFormatted);
+    
+                    $url = 'https://manhwacollection.com/manga/' . Str::slug($manhwa->name) . '/' . $slug;
+                    $response = $this->googleIndexingService->submitUrl($url);
+    
+                    if (isset($response['urlNotificationMetadata'])) {
+                        // Mark the chapter as indexed
+                        $index = IndexChapters::create([
+                            'chapter_id' => $chapter->id
+                        ]);
+                        $this->info($index . " Indexed: " . $url);
+    
+                        $chapter->is_indexed = true;
+                        $chapter->save();
+    
+                        Log::info($manhwa->id . " Indexed: " . $url);
+                        $this->info($manhwa->id . " Indexed: " . $url);
+                    } else {
+                        Log::info($manhwa->id . " Failed to index: " . $url);
+                        $this->error($manhwa->id . " Failed to index: " . $url);
+                    }
                 } else {
-                    Log::info($manhwa->id." Failed to index: " . $url);
-
-                    $this->error($manhwa->id." Failed to index: " . $url);
+                    Log::info($manhwa->id . " No new chapters to index for manhwa: " . $manhwa->name);
+                    $this->info($manhwa->id . " No new chapters to index for manhwa: " . $manhwa->name);
                 }
-            } else {
-                Log::info($manhwa->id." No new chapters to index for manhwa: " . $manhwa->name);
-
-                $this->info($manhwa->id." No new chapters to index for manhwa: " . $manhwa->name);
-            }
+            });
         }
     }
+    
 }
