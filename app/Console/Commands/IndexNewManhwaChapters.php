65376c724e95cbada874407997da333cf7b0956e
Maheer Akbar <maheerakbar@gmail.com>
2024-08-21 14:14:04 +0500

    changes

--- app/Console/Commands/IndexNewManhwaChapters.php
+++ app/Console/Commands/IndexNewManhwaChapters.php
@@ -37,41 +37,47 @@ class IndexNewManhwaChapters extends Command
         }
     
         foreach ($manhwaList as $manhwa) {
-            DB::transaction(function () use ($manhwa) {
-                // Fetch the latest chapter for each manhwa that hasn't been indexed yet
-                $chapter = $manhwa->chapters()
-                    ->orderByRaw('CAST(chapter_number AS UNSIGNED) DESC')
-                    ->first();
+            try {
+                DB::transaction(function () use ($manhwa) {
+                    // Fetch the latest chapter for each manhwa that hasn't been indexed yet
+                    $chapter = $manhwa->chapters()
+                        ->orderByRaw('CAST(chapter_number AS UNSIGNED) DESC')
+                        ->first();
     
-                if (!$chapter->is_indexed) {
-                    $chapterNumberFormatted = str_replace('.', '-', $chapter->chapter_number);
-                    $slug = Str::slug("Chapter " . $chapterNumberFormatted);
+                    if (!$chapter->is_indexed) {
+                        $chapterNumberFormatted = str_replace('.', '-', $chapter->chapter_number);
+                        $slug = Str::slug("Chapter " . $chapterNumberFormatted);
     
-                    $url = 'https://manhwacollection.com/manga/' . Str::slug($manhwa->name) . '/' . $slug;
-                    $response = $this->googleIndexingService->submitUrl($url);
+                        $url = 'https://manhwacollection.com/manga/' . Str::slug($manhwa->name) . '/' . $slug;
+                        $response = $this->googleIndexingService->submitUrl($url);
     
-                    if (isset($response['urlNotificationMetadata'])) {
-                        // Mark the chapter as indexed
-                        $index = IndexChapters::create([
-                            'chapter_id' => $chapter->id
-                        ]);
-                        $this->info($index . " Indexed: " . $url);
+                        if (isset($response['urlNotificationMetadata'])) {
+                            // Mark the chapter as indexed
+                            $index = IndexChapters::create([
+                                'chapter_id' => $chapter->id
+                            ]);
+                            $this->info($index . " Indexed: " . $url);
     
-                        $chapter->is_indexed = true;
-                        $chapter->save();
+                            $chapter->is_indexed = true;
+                            $chapter->save();
     
-                        Log::info($manhwa->id . " Indexed: " . $url);
-                        $this->info($manhwa->id . " Indexed: " . $url);
+                            Log::info($manhwa->id . " Indexed: " . $url);
+                            $this->info($manhwa->id . " Indexed: " . $url);
+                        } else {
+                            Log::info($manhwa->id . " Failed to index: " . $url);
+                            $this->error($manhwa->id . " Failed to index: " . $url);
+                        }
                     } else {
-                        Log::info($manhwa->id . " Failed to index: " . $url);
-                        $this->error($manhwa->id . " Failed to index: " . $url);
+                        Log::info($manhwa->id . " No new chapters to index for manhwa: " . $manhwa->name);
+                        $this->info($manhwa->id . " No new chapters to index for manhwa: " . $manhwa->name);
                     }
-                } else {
-                    Log::info($manhwa->id . " No new chapters to index for manhwa: " . $manhwa->name);
-                    $this->info($manhwa->id . " No new chapters to index for manhwa: " . $manhwa->name);
-                }
-            });
+                });
+            } catch (\Exception $e) {
+                Log::error("Failed to index manhwa: " . $manhwa->name . ". Error: " . $e->getMessage());
+                $this->error("Failed to index manhwa: " . $manhwa->name . ". Error: " . $e->getMessage());
+            }
         }
     }
+    
     
 }
